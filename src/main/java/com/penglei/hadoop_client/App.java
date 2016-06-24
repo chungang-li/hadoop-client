f41111c4159599a2b22ebf671963e828320d2f6b
kingLeiPeng <penglei_m@126.com>
2016-06-25 01:29:19 +0800

    初始化

---
+++ src/main/java/com/penglei/hadoop_client/App.java
@@ -0,0 +1,19 @@
+package com.penglei.hadoop_client;
+
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+		FileSystem fs = FileSystem.newInstance(new URI("hdfs://192.168.0.104:9000"), new Configuration());
+		
+		fs.create(new Path("/hello"));
+	}
+}
